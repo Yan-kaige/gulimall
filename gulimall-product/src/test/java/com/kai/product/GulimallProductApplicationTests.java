97d99946512414277e788a0e3ec41f6499531c0d
kaige <139201475@qq.com>
2023-07-22 10:10:17 +0800

    初始化

---
+++ gulimall-product/src/test/java/com/kai/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kai.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-wave/src/test/java/com/kai/wave/GulimallWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kai.wave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
